89e503add24e96c96b963e01e0d7f6128280e520
JianYao1992 <129933321+JianYao1992@users.noreply.github.com>
2023-04-06 12:28:03 +0800

    Add files via upload

---
+++ GetDataMatrixForMixedRepeatedAnova.m
@@ -0,0 +1,28 @@
+function Ps = GetDataMatrixForMixedRepeatedAnova(Data1, Data2, name)
+
+between_group = [];
+Mice_ID = [];
+within_group = [];
+performance = [];
+DayNum = length(Data1);
+for i=1:length(Data1{1}) % control group mice
+    between_group = [between_group; 1*ones(DayNum,1)];
+    Mice_ID = [Mice_ID; i*ones(DayNum,1)];
+    for j=1:DayNum % Day
+        within_group = [within_group; j];
+        performance = [performance; Data1{j}(i)];
+    end
+end
+
+for i=1:length(Data2{1}) % experimental group mice
+    between_group = [between_group; 2*ones(DayNum,1)];
+    Mice_ID = [Mice_ID; (i+length(Data1{1}))*ones(DayNum,1)];
+    for j=1:DayNum % Day
+        within_group = [within_group; j];
+        performance = [performance; Data2{j}(i)];
+    end
+end
+Frame = [ performance between_group within_group Mice_ID ];
+% Frame(31:38,:) = [];
+% Frame(31:35,4) = Frame(31:35,4) - 2;
+[SSQs, DFs, MSQs, Fs, Ps]=mixed_between_within_anova(Frame,0,[name '_stat']);
