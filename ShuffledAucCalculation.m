37d46f417f46d408ff943b390a992490a28c6b04
JianYao1992 <129933321+JianYao1992@users.noreply.github.com>
2023-05-09 14:47:55 +0800

    Add files via upload

---
+++ ShuffledAucCalculation.m
@@ -0,0 +1,19 @@
+function ShuffledAUC = ShuffledAucCalculation(FRinTwoKindsOfTrials,TrialNuminFirstKindOfTrial,SpecificWindow,TimeGain,IsDVOrFRROC)
+
+%% Step 1: Get the mean firing rate of all trials in two conditions when shuffled
+MeanFR_S1 = mean(FRinTwoKindsOfTrials{1}(:,(1+TimeGain*(SpecificWindow-1)):TimeGain*SpecificWindow),2);
+MeanFR_S2 = mean(FRinTwoKindsOfTrials{2}(:,(1+TimeGain*(SpecificWindow-1)):TimeGain*SpecificWindow),2);
+AllTrialMeanFR = [MeanFR_S1; MeanFR_S2];
+ShuffledOrder = randperm(size(AllTrialMeanFR,1));
+ShuffledMeanFR_S1 = AllTrialMeanFR(ShuffledOrder(1:TrialNuminFirstKindOfTrial),:);
+ShuffledMeanFR_S2 = AllTrialMeanFR(ShuffledOrder((1+TrialNuminFirstKindOfTrial):end),:);
+%% Step 2: Calculate the DV between S1 and S2
+if IsDVOrFRROC==1
+    [ShuffledDV_S1,ShuffledDV_S2] = CalculateDV(ShuffledMeanFR_S1,ShuffledMeanFR_S2);
+    %% Step 3: Calculate the TPR and FPR of single unit
+    [TPR,FPR] = CalculateTPRFPRValue(ShuffledDV_S1,ShuffledDV_S2);
+else
+    [TPR,FPR] = CalculateTPRFPRValue(ShuffledMeanFR_S1',ShuffledMeanFR_S2');
+end
+%% Step 4: Calculate the Area under curve (AUC) of each unit
+ShuffledAUC = CalculateAUCValue(FPR,TPR);
